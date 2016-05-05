f74c3137c845bdbdc7c3e67fc468756e1ead7cde
infuntis <infuntis@MacBook-Pro.local>
2016-05-05 22:54:57 +0300

    game completed

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import processing.core.PApplet;
+
+public class Main {
+
+    private final static String[] OPTIONS = new String[] {"--present", "Sketch"};
+
+    public static void main(String ... args){
+        PApplet.main(OPTIONS);
+    }
+
+}
